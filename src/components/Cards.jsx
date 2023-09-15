9f8886a07076b1da35e4f56d0c9a5d4498bbb9f5
Oneboy97 <bidinguiltetchuda@aluno.unilab.edu.br>
2023-09-14 23:10:24 -0300

    Criando Card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,11 +2,10 @@ import "./cardstaly.css"
 export function Cards() {
     return (
         <>
-
          <img src="assets\LogoToti.svg" alt="" /> 
 <h1> Full Stack </h1>
 <div className="quadradolaranja"> 
-    <div>
+    </div>
      <div>
        <img src="assets\Rectangle 2.svg" alt="" />
        <p>Projetos</p> 
@@ -18,6 +17,19 @@ export function Cards() {
        <p>20</p>
        <p>Aulas</p>
        </div>
+       <div className="cardlaranja">
+       </div>
+       <img src="assets/image 3.svg" alt="" />
+       <p>
+        <img src="assets/Rectangle 2 (1).svg" alt="" /> </p>
+        <div>
+        <p>Versionamento de Código</p>
+        <p>Kim Osinski</p>
+        <p>20</p>
+        <p>Aulas</p>
+        </div>
+
+       
     </div>
 </div>
         </>
