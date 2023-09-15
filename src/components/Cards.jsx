3851341704a626c9987612a8efd85a83a68d8ac3
Oneboy97 <bidinguiltetchuda@aluno.unilab.edu.br>
2023-09-14 22:45:22 -0300

    Criando Card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,16 +5,19 @@ export function Cards() {
 
          <img src="assets\LogoToti.svg" alt="" /> 
 <h1> Full Stack </h1>
-<div 
-className="quadradolaranja"> 
+<div className="quadradolaranja"> 
     <div>
+     <div>
        <img src="assets\Rectangle 2.svg" alt="" />
-       <p>Projetos</p>
+       <p>Projetos</p> 
+       </div>
+       <div>
        <p>Gerenciamento de Projetos </p>
        <p>Sophie Orellana</p>
        <p>Web Developer</p>
        <p>20</p>
        <p>Aulas</p>
+       </div>
     </div>
 </div>
         </>
