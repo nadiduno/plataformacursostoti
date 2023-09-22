69230371d5f52a73a3ff77cde237b9c15070266d
Nadi Duno <nadiduno@gmail.com>
2023-09-21 22:22:47 -0300

    Update

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -1,14 +1,11 @@
 import { Cards } from "../components/Card"
 import "../styles/Course.style.css"
+import "../styles/HomePage.style.css"
 
 export function Course() {
     return (
-        <div>
-            <img src="assets\LogoToti.svg" alt="" />
-            <h1> Full Stack </h1>
-            <div className="quadradolaranja">
-                <Cards />
-            </div>
-        </div >
+        <>
+            <h2>Login - Responsavel Tchuda</h2>
+        </>
     );
 }
