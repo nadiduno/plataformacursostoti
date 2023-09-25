e62e7227168dc91f3916e8f49fdb5ac0b364d574
123stack <raisonbip1999@gmail.com>
2023-09-25 20:09:41 -0300

    agora vai

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,19 @@
 import "../styles/Login.style.css"
-import "../styles/HomePage.style.css"
 
 export function Login() {
     return (
         <>
             <h2>Login - Responsavel Jacky</h2>
+            <p> Meu primeiro passo </p>
+            <main>
+            <div className="textWelcome">
+                <h1>Bem vindo</h1>
+                <p>Faça login ou cria sua conta</p>
+            </div>
+            <div className="buttonText TextBlack"> 
+            </div>
+           <p>Oi Tchuda</p> 
+            </main>
         </>
     )
 }
