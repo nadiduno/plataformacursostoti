7ddf6ff317e20bf190351c8152b27046470a7f60
Oneboy97 <bidinguiltetchuda@aluno.unilab.edu.br>
2023-09-25 23:47:55 -0300

    Atualizando card

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -4,42 +4,49 @@ import "../styles/HomePage.style.css"
 
 export function Course() {
     return (
-       
-     <div className="containerMain">
-        <div className=" container">
-       <aside  className="sideBar" >
-            <img src="src\assets\LogoToti.svg" alt="" />
-       </aside>
-               <div>
-                <img src="src\assets\Full Stack.svg" alt="" />
-               </div>
-               <div className="boxOrange">
-               <h1><img src="src\assets\Rectangle 2.svg" alt="" /></h1> 
-               <h1> Projetos </h1>
-               <p>Gerenciamento de Projetos
-               </p>
-               <img src="src\assets\Ellipse 1.svg" alt="" />
-               Sophie Orellana
-               <p>Web Developer</p>  
-               <ol>20</ol>
-               <p>Aulas </p>
-               </div>
-               </div>
+
+        <div className="containerMain">
+            <div className=" container">
+                <aside className="sideBar" >
+                    <img src="src\assets\LogoToti.svg" alt="" />
+                </aside>
+                <div className="containerCentral">
+                    <div>
+                        <h1>Full Stack </h1>
+                    </div>
+                    <div className="boxOrange">
+                        <div className="cardTop">
+                            <img src="src\assets\Rectangle 2.svg" alt="" />
+
+                            <p className="textCard"> Projetos </p>
+                        </div>
+                        <div className="cardBottom">
+                            <p>Gerenciamento de Projetos
+                            </p>
+                            <img src="src\assets\Ellipse 1.svg" alt="" />
+                            Sophie Orellana
+                            <p>Web Developer</p>
+                            <p>20</p>
+                            <p>Aulas </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <footer>
-           <a href="https://totidiversidade.com.br/"
-            target="_blank"
-            className="linkHover"
-           >
-           Toti Diversidade
-           </a>
-           <span>Todos os direitos reservados  - 2023 </span>
-           <div className="logoSocial">        
-           <img src="src\assets\Logos.svg" alt="" />
+                <a href="https://totidiversidade.com.br/"
+                    target="_blank"
+                    className="linkHover"
+                >
+                    Toti Diversidade
+                </a>
+                <span>Todos os direitos reservados  - 2023 </span>
+                <div className="logoSocial">
+                    <img src="src\assets\Logos.svg" alt="" />
                 </div>
             </footer>
 
-      </div>            
-          
+        </div>
+
     );
 }
 
