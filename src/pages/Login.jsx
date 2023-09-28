a427679ba7a2477989012b7c10e06b4c8278f3cd
123stack <raisonbip1999@gmail.com>
2023-09-27 21:31:06 -0300

    step one

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,45 @@
 import "../styles/Login.style.css"
-
+import "../styles/HomePage.style.css"
 export function Login() {
     return (
-        <>
-            <h2>Login - Responsavel Jacky</h2>
-            <p> Meu primeiro passo </p>
-            <main>
+        
+            
+        <body>
+            <div className="containerMain">
+                <div className="container">
+                    <aside className="sideBar">
+                        <img className="sideImage" 
+                        src="src\assets\asidephoto.svg" alt=""/>
+                    </aside>
+                </div>
+            <main className="containerCentral">
+              <div className="boxOrange">
+                <div className="textWelcome">
+              <h1> Bemvindo ao Curso de programação da TOTI</h1>
+              <p><h2>Acessa sua conta</h2></p>
+              </div>   
+              <div className="boxViolet">          
+               <form>
+                <label for="userName"><h2>Nome de usuário ou e-mail:</h2></label><br></br>
+                <input type="text" id="userName" name="userName"> </input><br></br>
+
+                </form>
+                </div>
+                </div>  
+            </main> 
+            <div className="container">
+                
             <div className="textWelcome">
                 <h1>Bem vindo</h1>
                 <p>Faça login ou cria sua conta</p>
             </div>
-            <div className="buttonText TextBlack"> 
+            <div className="buttonText TextRed"> 
             </div>
-           <p>Oi Tchuda</p> 
-            </main>
-        </>
+           <p>Oi Tchuda</p>
+           <p> ver atualização</p> 
+           </div>
+           
+            </div>
+        </body>
     )
 }
