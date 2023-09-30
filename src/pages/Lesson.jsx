58ede04b97be464381dc6db03aea513354aea1e8
Nadi Duno <nadiduno@gmail.com>
2023-09-30 03:13:08 -0300

    Page Lesson

--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -1,156 +1,178 @@
 import React, { useState, useEffect } from 'react';
-import { Accordion, Button, Form, ListGroup } from 'react-bootstrap';
+import { Button, Col, Form, InputGroup, Row, ListGroup, Modal, ButtonGroup } from 'react-bootstrap';
+import { ListLesson } from '../components/ListLesson';
+import { GithubLogo, InstagramLogo, LinkedinLogo, NotePencil, Plus, UserCircle } from "phosphor-react"
+import "../styles/HomePage.style.css"
+
 import "../styles/Lesson.style.css"
 
-export function Lesson() {
-    const [lessons, setLessons] = useState([]);
-    const [id, setId] = useState(null);
-    const [lesson, setLesson] = useState(null);
+export const Lesson = () => {
+  const [lessons, setLessons] = useState([]);
+  const [showAdicionar, setShowAdicionar] = useState(false);
+  const [state, setState] = useState({ title: '', description: '' });
+  const [validated, setValidated] = useState(false);
 
-    // Formulário para criar um lesson
-    const [titulo, setTitulo] = useState('');
-    const [descricao, setDescricao] = useState(null);
-    const [publicado, setPublicado] = useState(false);
+  useEffect(() => {
+    trazerLista();
+  }, []);
 
-    const [novaDescricao, setNovaDescricao] = useState(null);
+  const trazerLista = () => {
+    fetch('http://localhost:9000/api/lessons/published')
+      .then(data => data.json())
+      .then(resposta => {
+        setLessons(resposta);
+      })
+      .catch(err => console.log('Erro de solicitação', err));
+  };
 
-    useEffect(() => {
-        console.log('vai executar em qualquer evento da tela');
-        // requisição, click de um botao que tem alguma ação, mudança no input
+  const adicionarLesson = () => {
+    setShowAdicionar(true);
+  };
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity()) { // caso de sucesso
+      setLessons(anterior => {
+        let lessonsAntigos = anterior;
+        lessonsAntigos.push({ id: lessons.length + 1, title: state.title, description: state.description });
+        return lessonsAntigos;
+      });
+      setShowAdicionar(false);
+    }
+    event.preventDefault();
+    event.stopPropagation();
+    setValidated(true);
+  };
+
+  const handleChange = e => {
+    setState(anterior => {
+      return {
+        ...anterior,
+        [e.target.name]: e.target.value
+      }
     });
+  };
 
-    useEffect(() => {
-        trazerLista();
-    }, []); // só vai ser executado no primeiro load da página (carregar a primeira vez)
+  const buscarlesson = e => {
+    fetch(`https://dummyjson.com/products/search?q=${e.target.value}`)
+      .then(data => data.json())
+      .then(resposta => {
+        setLessons(resposta.products);
+      })
+      .catch(err => console.log('Erro de solicitação', err));
+  };
 
-    useEffect(() => {
-        console.log('titulo mudou');
-    }, [titulo]); // vai ser executado sempre que a variável mudar de valor
+  return (
+    <div className="containerMain">
+      <div className="container">
+        <aside className="sideBar">
+          <img src="src\assets\LogoToti.svg" alt="" />
+        </aside>
+        <main className="containerCentral">
+          <div className="boxOrange">
+            <div style={{ padding: '30px' }}>
+              
+              <button className="buttonViolet linkHover" onClick={adicionarLesson}>
+                <NotePencil size={24} />
+                Adicionar Aula
+              </button>
+              <InputGroup className="mb-3 pt-3">
+                <InputGroup.Text id="basic-addon1">Buscar</InputGroup.Text>
+                <Form.Control
+                  placeholder="Nome"
+                  aria-label="nome"
+                  aria-describedby="basic-addon1"
+                  onChange={(e) => buscarlesson(e)}
+                />
+              </InputGroup>
 
-    const trazerLista = () => {
-        // fetch('https://dummyjson.com/products')
-        fetch('http://localhost:9000/api/lessons/published')
-            .then(data => data.json())
-            .then(resposta => {
+              {
+                lessons.length ? <ListLesson lista={lessons} /> : <></>
+              }
 
-                console.log(resposta)
-                setLessons(resposta);
-            })
-            .catch(err => console.log('Erro de solicitação', err));
-    };
+              <Modal show={showAdicionar} onHide={() => setShowAdicionar(false)}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Adicionar lesson</Modal.Title>
+                </Modal.Header>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                  <Modal.Body>
+                    <Form.Group>
+                      <Form.Label>Titulo</Form.Label>
+                      <InputGroup hasValidation>
+                        <Form.Control
+                          name="title"
+                          type="text"
+                          placeholder="Titulo"
+                          onChange={handleChange}
+                          required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          Campo obrigatório.
+                        </Form.Control.Feedback>
+                      </InputGroup>
+                    </Form.Group>
+                    <Form.Group className="pt-3">
+                      <Form.Label>Descrição</Form.Label>
+                      <InputGroup hasValidation>
+                        <Form.Control
+                          name="description"
+                          type="text"
+                          placeholder="Descrição"
+                          onChange={handleChange}
+                          required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          Campo obrigatório.
+                        </Form.Control.Feedback>
+                      </InputGroup>
+                    </Form.Group>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="primary" type="submit">Criar</Button>
+                    <Button variant="secondary" onClick={() => setShowAdicionar(false)}>Cancelar</Button>
+                  </Modal.Footer>
+                </Form>
+              </Modal>
+              <div className="pt-5"></div>
+            </div>
+          </div>
+        </main>
+      </div>
+      <footer>
+        <a
+          href="https://totidiversidade.com.br/"
+          target="_blank"
+          className="linkHover"
+        >
+          Toti Diversidade
+        </a>
+        <span>Todos os direitos reservados  - 2023 </span>
+        <div className="logoSocial">
+          <a
+            href="https://github.com/toti-edu"
+            target="_blank"
+            className="linkHover"
+          >
+            <GithubLogo size={30} />
+          </a>
+          <a
+            href="https://www.instagram.com/toti.diversidade/?hl=pt-br"
+            target="_blank"
+            className="linkHover"
+          >
+            <InstagramLogo size={30} />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/toti-diversidade/"
+            target="_blank"
+            className="linkHover"
+          >
+            <LinkedinLogo size={30} />
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
-    const criarLesson = () => {
-        const parametros = {
-            title: titulo,
-            description: descricao,
-            published: publicado
-        };
 
-        fetch('http://localhost:9000/api/lessons', {
-            method: "POST",
-            body: JSON.stringify(parametros),
-            headers: { "Content-type": "application/json; charset=UTF-8" }
-        })
-            .then(data => data.json())
-            .then(response => trazerLista())
-            .catch(err => console.log(err));
-    };
-
-    const buscarPorID = () => {
-        const URL = 'http://localhost:9000/api/lessons/' + id;
-        fetch(URL)
-            .then(data => data.json())
-            .then(resposta => {
-                if (resposta.title) {
-                    setLesson(resposta);
-                } else {
-                    alert(resposta.message)
-                }
-            })
-            .catch(err => console.log('Erro de solicitação', err));
-    };
-
-    const deletarPorID = () => {
-        const URL = 'http://localhost:9000/api/lessons/' + id;
-        fetch(URL, {
-            method: "DELETE"
-        })
-            .then(data => data.json())
-            .then(response => trazerLista())
-            .catch(err => console.log(err));
-    };
-
-    const atualizarPorID = () => {
-        const URL = 'http://localhost:9000/api/lessons/' + id;
-        const atualizarlesson = {
-            description: novaDescricao
-        };
-        fetch(URL, {
-            method: "PUT", // PATCH
-            body: JSON.stringify(atualizarlesson),
-            headers: { "Content-type": "application/json; charset=UTF-8" }
-        })
-            .then(data => data.json())
-            .then(response => trazerLista())
-            .catch(err => console.log(err));
-    };
-
-    return (
-        <div style={{ padding: '30px' }}>
-
-            <Accordion defaultActiveKey="0" flush alwaysOpen>
-                <Accordion.Item eventKey="0" title="Clique-me para abrir e fechar o acordeon">
-                    <Accordion.Header>Adicionar Produto</Accordion.Header>
-                    <Accordion.Body>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Produto</Form.Label>
-                            <Form.Control style={{ width: '20rem' }} type="text" onChange={(e) => setTitulo(e.target.value)} placeholder="Escrever um nome para o produto" />
-                            <Form.Label>Descrição</Form.Label>
-                            <Form.Control style={{ width: '20rem' }} type="text" onChange={(e) => setDescricao(e.target.value)} placeholder="Escrever descrição para o produto" />
-                            <Form.Check type="checkbox" onClick={(e) => setPublicado(e.target.checked)} label="Publicado" />
-                            <Button onClick={criarLesson}>Criar</Button>
-                        </Form.Group>
-                    </Accordion.Body>
-                </Accordion.Item>
-
-                <Accordion.Item eventKey="1" title="Clique-me para abrir e fechar o acordeon">
-                    <Accordion.Header>Atualizar Produto</Accordion.Header>
-                    <Accordion.Body>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>ID</Form.Label>
-                            <Form.Control style={{ width: '20rem' }} type="number" onChange={(e) => setId(e.target.value)} placeholder="Escrever o ID do produto" />
-                            <Form.Label>Nova Descrição</Form.Label>
-                            <Form.Control style={{ width: '20rem' }} type="text" onChange={(e) => setNovaDescricao(e.target.value)} placeholder="Escrever a nova descrição para o produto" />
-                            <Button style={{ marginTop: '.5rem' }} onClick={atualizarPorID}>Atualizar</Button>
-                        </Form.Group>
-                    </Accordion.Body>
-                </Accordion.Item>
-
-                <Accordion.Item eventKey="2" title="Clique-me para abrir e fechar o acordeon">
-                    <Accordion.Header>Eliminar Produto</Accordion.Header>
-                    <Accordion.Body>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Eliminar por ID</Form.Label>
-                            <Form.Control style={{ width: '20rem' }} type="number" onChange={(e) => setId(e.target.value)} placeholder="Escrever o ID do produto" />
-                            <Button style={{ marginTop: '.5rem' }} onClick={deletarPorID}>Deletar</Button>
-                        </Form.Group>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
-
-            <hr /><br /><br />
-            <h3 style={{ color: "var(--color3)" }}>Listado de Aulas</h3>
-            <br />
-            <ListGroup>
-                {
-                    lessons.length ?
-                        lessons.map(lesson => {
-                            return <ListGroup.Item key={lesson.id}>{lesson.id} - {lesson.title} - {lesson.description}</ListGroup.Item>
-                        })
-                        : <div></div>
-                }
-            </ListGroup>
-            <br /><br />
-        </div >
-    )
-}
